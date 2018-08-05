5feca6f107e4542b682018dd535a9c3ced67c91d
zmjzzz <zmjzz@163.com>
2018-08-05 19:07:24 +0800

    mycode version1.2 tensorflow:helloworld

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -3,6 +3,15 @@ import numpy as np
 import time
 import datetime
 start_time = time.time()
-tf.Session()
 end_time = time.time()
+
+matrix1 = tf.constant([[3., 3.]])
+
+matrix2 = tf.constant([[2.], [2.]])
+
+product = tf.matmul(matrix1, matrix2)
+sess = tf.Session()
+result = sess.run(product)
+print (result)
+sess.close()
 
